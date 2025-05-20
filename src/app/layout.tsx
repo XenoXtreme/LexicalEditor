88f374bc5f47b5d7db96035621cc412143be704b
Sinchan Maitra <maitrababai2007@gmail.com>
2025-05-20 12:19:17 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Runtime Error: Error: ./src/app/layout.tsx
    > Module not found: Can't resolve 'next/font/google/target.css'
    > Unknown font
    >
    > Debug info:
    > - Execution of <ModuleAssetContext as AssetContext>::resolve_asset failed
    > - Execution of resolve failed
    > - Execution of resolve_internal failed
    > - Execution of NextFontGoogleReplacer::import_map_result failed
    > - Execution of font_options_from_query_map failed
    > - Unknown font
    > Error while looking up import map: Unknown font
    >
    > Debug info:
    > - Execution of NextFontGoogleReplacer::import_map_result failed
    > - Execution of font_options_from_query_map failed
    > - Unknown font
    >
    >
    > https://nextjs.org/docs/messages/module-not-found
    >
    > . Error source: Call Stack
    > 7
    >
    > Show 7 ignore-listed frame(s)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,19 @@
 
 import type {Metadata} from 'next';
-// Use the correct exported names: GeistSans and GeistMono (camelCase)
-import { GeistSans, GeistMono, Roboto, Open_Sans, Lato, Montserrat } from 'next/font/google';
+// Temporarily remove GeistSans and GeistMono to isolate the font loading issue.
+// import { GeistSans, GeistMono, Roboto, Open_Sans, Lato, Montserrat } from 'next/font/google';
+import { Roboto, Open_Sans, Lato, Montserrat } from 'next/font/google';
 import './globals.css';
 
-const geistSans = GeistSans({ // Use GeistSans (camelCase)
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
+// const geistSans = GeistSans({
+//   variable: '--font-geist-sans',
+//   subsets: ['latin'],
+// });
 
-const geistMono = GeistMono({ // Use GeistMono (camelCase)
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+// const geistMono = GeistMono({
+//   variable: '--font-geist-mono',
+//   subsets: ['latin'],
+// });
 
 const roboto = Roboto({
   variable: '--font-roboto',
@@ -50,7 +51,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} ${roboto.variable} ${openSans.variable} ${lato.variable} ${montserrat.variable} antialiased`}>
+      <body className={`${roboto.variable} ${openSans.variable} ${lato.variable} ${montserrat.variable} antialiased`}>
+        {/* Removed geistSans.variable and geistMono.variable */}
         {children}
       </body>
     </html>
